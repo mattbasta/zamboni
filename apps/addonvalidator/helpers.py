f0e820dbe7babf372e15267eec8ac1371edb17e5
Matt Basta <mbasta@mozilla.com>
2010-08-09 12:50:39 -0700

    L10n stuff fixes

--- apps/addonvalidator/helpers.py
+++ apps/addonvalidator/helpers.py
@@ -13,16 +13,18 @@ def print_file(context, filename, line=None):
             filename[-1] = _("(no file)")
         output = "%s"
         for name in filename:
+            # L10n: %s is the file name, ignore %%s
             output = output % ('<span class="line">%s%%s</span>' % name)
         if line is not None:
-            output = output % " @ Line %s" % line
+            # L10n: %s is a line number
+            output = output % _(" @ Line %s") % line
         else:
             output = output % ""
 
         return jinja2.Markup(output)
     else:
         if not filename:
-            return "(no file)"
+            return _("(no file)")
         return filename
 
 
@@ -34,7 +36,7 @@ def print_description(context, description):
     if isinstance(description, list):
         output = []
         for line in description:
-            output.append(_(line))
+            output.append(line)
 
         return jinja2.Markup("</p><p>".join(output))
     else:
@@ -75,24 +77,24 @@ def result_class(rejected, success):
 
 @register.function
 def translate_name(name):
-    translation = {"chromemanifest": "Chrome Manifest",
-                   "main": "General Tests",
-                   "rdf": "RDF Tests",
-                   "typedetection": "Add-on Type Detection",
-                   "xpi": "XPI Parser",
-                   "testcases_conduit": "Conduit Testing",
-                   "testcases_content": "Package Content",
-                   "testcases_installrdf": "install.rdf Tests",
-                   "testcases_l10ncompleteness": "L10n Completeness",
-                   "testcases_langpack": "Language Pack Tests",
-                   "testcases_library_blacklist": "Library Blacklists",
-                   "testcases_packagelayout": "Package Layout",
-                   "testcases_targetapplications": "Target Application Tests",
-                   "testcases_themes": "Theme Tests",
-                   "testcases_l10n_dtd": "DTD File Tests",
-                   "testcases_l10n_properties": "Properties File Tests",
-                   "testcases_markup_csstester": "CSS Tests",
-                   "testcases_markup_markuptester": "Markup Tests"}
+    translation = {"chromemanifest": _("Chrome Manifest"),
+                   "main": _("General Tests"),
+                   "rdf": _("RDF Tests"),
+                   "typedetection": _("Add-on Type Detection"),
+                   "xpi": _("XPI Parser"),
+                   "testcases_conduit": _("Conduit Testing"),
+                   "testcases_content": _("Package Content"),
+                   "testcases_installrdf": _("install.rdf Tests"),
+                   "testcases_l10ncompleteness": _("L10n Completeness"),
+                   "testcases_langpack": _("Language Pack Tests"),
+                   "testcases_library_blacklist": _("Library Blacklists"),
+                   "testcases_packagelayout": _("Package Layout"),
+                   "testcases_targetapplication": _("Target Application Tests"),
+                   "testcases_themes": _("Theme Tests"),
+                   "testcases_l10n_dtd": _("DTD File Tests"),
+                   "testcases_l10n_properties": _("Properties File Tests"),
+                   "testcases_markup_csstester": _("CSS Tests"),
+                   "testcases_markup_markuptester": _("Markup Tests")}
     if name in translation:
-        return _(translation[name])
+        return translation[name]
     return name
